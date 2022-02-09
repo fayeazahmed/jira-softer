4462199070e3c8f715cabed5ae1777e2fed83759
fayeazahmed <r10.ahmed@yahoo.com>
2022-02-09 19:58:37 +0600

    v1.0

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const api = axios.create();
+const api = axios.create({
+    baseURL: "/api",
+  });
 
 export const setAuthToken = (token = localStorage.getItem("token")) => {
     if (token) api.defaults.headers.common["Authorization"] = "Bearer " + token;
